805695f248939354ad2f20f2ee100877738e730d
Vasco <vasco.weyne@gmail.com>
2022-11-15 17:53:54 -0300

    atualizando index e script.js

--- front/index.js
+++ front/index.js
@@ -1,10 +1,70 @@
-$.get("http://localhost:3000/produto", function (data) {}).done(function (
-  data
-) {
-  data.forEach((element) => {
-    console.log(element);
-    document.querySelector(
-      "#products-list"
-    ).innerHTML += `<li>${element.nome}, ${element.id}</li>`;
-  });
-});
+const createB = document.querySelector('#create')
+
+createB.addEventListener('click', () => {
+  const create = getCreateForm()
+
+  criarProduto(create)
+})
+
+
+function getCreateForm(){
+  const inputCategoria = document.querySelector('#categoria')
+  const inputProduto = document.querySelector('#produto')
+  if (inputCategoria.value === null || inputProduto === null){
+    console.log('campos vazio')
+    return;
+  }
+  const create = {
+    categoria: inputCategoria.value,
+    produto: inputProduto.value
+  }
+}
+
+async function criarProduto(create) {
+  const resposta = await fetch('http://localhost:3000/produto', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(create)
+  })
+   if(resposta.status === 201){
+     limparCampos()
+     window.location.href = 'home.html'
+   }else{
+     console.log('error');
+   }
+  console.log(resposta);
+}
+
+async function lerProduto(create) {
+  const resposta = await fetch('http://localhost:3000/produto', {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(create)
+  })
+   if(resposta.status === 201){
+     limparCampos()
+     window.location.href = 'home.html'
+   }else{
+     console.log('error');
+   }
+  console.log(resposta);
+}
+
+
+//  async function produto (){$.get("http://localhost:3000/produto", function (data) {}).done(function (
+//    data
+//  ) {
+//    data.forEach((element) => {
+//      console.log(element);
+//      document.querySelector(
+//        "#products-list"
+//      ).innerHTML += `<li>${element.nome}, ${element.id}</li>`;
+//    });
+//  });
+//  }
