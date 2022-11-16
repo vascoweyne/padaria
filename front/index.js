67b8887da3cc96cbda0f9ae5aa3822815b6189cf
Vasco <vasco.weyne@gmail.com>
2022-11-15 23:30:45 -0300

    alterações para melhoria

--- front/index.js
+++ front/index.js
@@ -1,64 +1,89 @@
-const createB = document.querySelector('#create')
+function fetchApiDataCategoria(){
+    fetch('http://localhost:3000/categoria')
+    .then(response => response.json())
+    .then(data => {
+      const li = document.querySelector('#lista')
 
-createB.addEventListener('click', () => {
-  const create = getCreateForm()
+      data.map((item) => {
+        const li = document.createElement('li')
 
-  criarProduto(create)
-})
+        li.setAttribute('nome'.item.nome)
+        li.innerHTML = item.nome;
+        list.appendChild
+      }) 
+    })
 
+ }
 
-function getCreateForm(){
-  const inputCategoria = document.querySelector('#categoria')
-  const inputProduto = document.querySelector('#produto')
-  if (inputCategoria.value === null || inputProduto === null){
-    console.log('campos vazio')
-    return;
-  }
-  const create = {
-    categoria: inputCategoria.value,
-    produto: inputProduto.value
-  }
+ function fetchApiDataProduto(){
+  fetch('https://localhost:3000/produto')
+  .then ((response) => response.json())
+  .then((data) => console.log(data));
+
 }
 
-async function criarProduto(create) {
-  const resposta = await fetch('http://localhost:3000/produto', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(create)
-  })
-   if(resposta.status === 201){
-     limparCampos()
-     window.location.href = 'home.html'
-   }else{
-     console.log('error');
-   }
-  console.log(resposta);
-}
 
-async function lerProduto(create) {
-  try {
-  const resposta = await fetch('http://localhost:3000/produto', {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(create)
-  })
-   if(resposta.status === 201){
-     limparCampos()
-     window.location.href = 'home.html'
-   }else{
-     console.log('error');
-   }
-   }catch (error) {
-      console.log(error);
-   }
-  console.log(resposta);
-}
+// const createB = document.querySelector('#create')
+
+// createB.addEventListener('click', () => {
+//   const create = getCreateForm()
+
+//   criarProduto(create)
+// })
+
+
+// function getCreateForm(){
+//   const inputCategoria = document.querySelector('#categoria')
+//   const inputProduto = document.querySelector('#produto')
+//   if (inputCategoria.value === null || inputProduto === null){
+//     console.log('campos vazio')
+//     return;
+//   }
+//   const create = {
+//     categoria: inputCategoria.value,
+//     produto: inputProduto.value
+//   }
+// }
+
+// async function criarProduto(create) {
+//   const resposta = await fetch('http://localhost:3000/produto', {
+//     method: 'POST',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(create)
+//   })
+//    if(resposta.status === 201){
+//      limparCampos()
+//      window.location.href = 'home.html'
+//    }else{
+//      console.log('error');
+//    }
+//   console.log(create);
+// }
+
+// async function lerProduto(create) {
+//   try {
+//   const resposta = await fetch('http://localhost:3000/produto', {
+//     method: 'GET',
+//     headers: {
+//       Accept: 'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(create)
+//   })
+//    if(resposta.status === 201){
+//      limparCampos()
+//      window.location.href = 'home.html'
+//    }else{
+//      console.log('error');
+//    }
+//    }catch (error) {
+//       console.log(error);
+//    }
+//   console.log(resposta);
+// }
 
 
 
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(port);
+  await app.listen(process.env.PORT || 3000);
   console.log(`App Rodando na porta ${port}`);
 }
 bootstrap();
